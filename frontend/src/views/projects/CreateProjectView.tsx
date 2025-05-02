0247a2f6939d117b81a7f98659c72f41390ec96e
Daniel Molina <danielmolina.dev@gmail.com>
2025-05-02 21:32:59 +0200

    refactor: add react query

--- frontend/src/views/projects/CreateProjectView.tsx
+++ frontend/src/views/projects/CreateProjectView.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
+import { useMutation } from "@tanstack/react-query";
 import { ProjectFormData } from "@/types";
 import ProjectForm from "@/components/projects/ProjectForm";
 import { createProject } from "@/api/ProjectAPI";
@@ -19,11 +20,16 @@ export default function CreateProjectView() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const handleForm = async (formData: ProjectFormData) => {
-    const data = await createProject(formData);
-    toast.success(data);
-    navigate("/");
-  };
+  const { mutate } = useMutation({
+    mutationFn: createProject,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data);
+      navigate("/");
+    },
+  });
+
+  const handleForm = (formData: ProjectFormData) => mutate(formData);
 
   return (
     <>
